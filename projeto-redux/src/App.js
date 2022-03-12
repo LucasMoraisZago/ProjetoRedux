22bb6257d46dae2f85b1e798734ccc2ceb3127f0
LucasMoraisZago <lucasmoraiszago@hotmail.com>
2022-03-12 14:43:17 +0000

    Aula 12/03/2022

---
+++ projeto-redux/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Card from './components/Card';
+
+function App(){
+  return(
+    <div className='App'>
+      <h1>Projeto Redux</h1>
+      <div className='linha'>
+        <Card/>   <Card/>
+      </div>
+      <div className='linha'>
+        <Card/>   <Card/>
+      </div>
+    </div>
+  )
+}
+export default App;
---
+++ projeto-redux/src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Cards.css'
+
+export default props=> {
+    return (
+        <div>
+            Card
+        </div>
+    )
+}
